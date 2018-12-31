278388a01ad652bef5c64081f54076a6d96cdaec
JohnCoene <jcoenep@gmail.com>
2018-12-31 16:59:07 +0100

    redone site

--- R/tag.R
+++ R/tag.R
@@ -6,6 +6,28 @@
 #' @param version Version to pass to \link[aframer]{a_scene}.
 #' @param ... any \code{aframer} components.
 #'
+#' @details Valid \code{preset}:
+#' \itemize{
+#'   \item{\code{none}}
+#'   \item{\code{default}}
+#'   \item{\code{contact}}
+#'   \item{\code{egypt}}
+#'   \item{\code{checkboard}}
+#'   \item{\code{forest}}
+#'   \item{\code{goaland}}
+#'   \item{\code{goldmine}}
+#'   \item{\code{threetowers}}
+#'   \item{\code{poison}}
+#'   \item{\code{arches}}
+#'   \item{\code{tron}}
+#'   \item{\code{japan}}
+#'   \item{\code{dream}}
+#'   \item{\code{volcano}}
+#'   \item{\code{starry}}
+#'   \item{\code{osiris}}
+#'   \item{\code{yavapai}}
+#' }
+#'
 #' @examples
 #' library(aframer)
 #'
